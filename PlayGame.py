27fc4907be690e383d38a7c2158b07c40251ebd5
behrouz.ghamkhar <behrouz.ghamkhar@partsoftware.com>
2023-04-07 20:00:57 +0330

    Changed variables name

--- PlayGame.py
+++ PlayGame.py
@@ -10,20 +10,20 @@ class PlayGame:
     def __init__(self):
         NotImplemented
 
-    def play(self):
+    def play_konane(self):
         size = 6
-        konane_game = KonaneGame()
-        initial_board = Board(size, konane_game.initialize_board(size))
-        agent1 = Agent(konane_game, color=Tile.P_Black, max_depth=4)
-        agent2 = Agent(konane_game, color=Tile.P_White, max_depth=2)
+        game = KonaneGame()
+        initial_board = Board(size, game.initialize_board(size))
+        agent1 = Agent(game, color=Tile.P_Black, max_depth=4)
+        agent2 = Agent(game, color=Tile.P_White, max_depth=2)
         # bot vs bot
-        game = PlayKonane(initial_board, konane_game, agent1=agent1, agent2=agent2)
+        game = PlayKonane(initial_board, game, agent1=agent1, agent2=agent2)
 
         # player vs bot
-        #game = PlayKonane(initial_board, konane_game, agent1=agent2)
+        #game = PlayKonane(initial_board, game, agent1=agent2)
     
 
 
 
 if __name__ == '__main__':
-    PlayGame().play()
+    PlayGame().play_konane()
