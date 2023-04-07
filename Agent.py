5c383c67fdc8344b3f7d34da45095a2f85e327df
behrouz.ghamkhar <behrouz.ghamkhar@partsoftware.com>
2023-04-07 19:45:19 +0330

    Changed parametes name

--- Agent.py
+++ Agent.py
@@ -5,8 +5,8 @@ class Agent:
     MIN_VALUE = -1000000
     MAX_VALUE= 1000000
 
-    def __init__(self, konane_game, color, max_depth):
-        self.konane_game = konane_game
+    def __init__(self, game, color, max_depth):
+        self.game = game
         self.color = color
         self.max_depth = max_depth
     
@@ -19,17 +19,17 @@ class Agent:
 
     def max(self, current_board, current_color, depth):
        
-        if (self.konane_game.check_terminal(current_board, current_color)):
-            return None, self.konane_game.evaluate(current_board, current_color, -1000)
+        if (self.game.check_terminal(current_board, current_color)):
+            return None, self.game.evaluate(current_board, current_color, -1000)
 
         if (depth == self.max_depth):
-            return None, self.konane_game.evaluate(current_board, current_color)
+            return None, self.game.evaluate(current_board, current_color)
 
-        possible_moves =  self.konane_game.generate_all_possible_moves(current_board, current_color)
+        possible_moves =  self.game.generate_all_possible_moves(current_board, current_color)
         best_move = None
         best_move_value = self.MIN_VALUE
         for move in possible_moves:
-            temp_move, value = self.min(current_board.next_board(current_color, move), self.konane_game.opponent(current_color), depth + 1, alpha, beta)
+            temp_move, value = self.min(current_board.next_board(current_color, move), self.game.opponent(current_color), depth + 1, alpha, beta)
             if (value > best_move_value):
                 best_move_value = value
                 best_move = move
